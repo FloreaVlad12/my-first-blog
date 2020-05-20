0dd3d8e5b9317f0c4c7a0063b9c1934565dd0703
FloreaVlad <vlad.dimitrie.florea@gmail.com>
2020-05-20 15:15:42 +0300

    Added Korean language option

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.db.models import Count
 
 
 class Post(models.Model):
@@ -9,6 +10,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+   
     
 
     def publish(self):
--- blog/views.py
+++ blog/views.py
@@ -84,3 +84,80 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
+
+def kr_post_list(request):
+     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+     return render(request, 'blog/kr_post_list.html', {'posts': posts})
+ 
+def kr_post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/kr_post_detail.html', {'post': post})
+
+@login_required
+def kr_post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('kr_post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/kr_post_edit.html', {'form': form})
+
+@login_required
+def kr_post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('kr_post_list')
+
+def kr_add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('kr_post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/kr_add_comment_to_post.html', {'form': form})
+
+@login_required
+def kr_comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('kr_post_detail', pk=comment.post.pk)
+
+@login_required
+def kr_comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('kr_post_detail', pk=comment.post.pk)
+
+@login_required
+def kr_post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('kr_post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/kr_post_edit.html', {'form': form})
+
+@login_required
+def kr_post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('kr_post_detail', pk=pk)
+
+@login_required
+def kr_post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/kr_post_draft_list.html', {'posts': posts})
