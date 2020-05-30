1b08ab3d7b4bb5e59ec146a87f43df020cb02777
FloreaVlad <vlad.dimitrie.florea@gmail.com>
2020-05-30 18:51:44 +0300

    added priority

--- blog/views.py
+++ blog/views.py
@@ -10,8 +10,8 @@ from django.core.mail import send_mail
 
 # Create your views here.
 def post_list(request):
-     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-     return render(request, 'blog/post_list.html', {'posts': posts})
+     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-priority', '-published_date' )
+     return render(request, 'blog/post_list.html', {'posts': posts})  
  
 
 def post_detail(request, pk):
